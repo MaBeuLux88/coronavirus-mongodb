9ddcbbdb0354e1cc0753a78fafa7e19085ad95ff
Maxime Beugnet <maxime.beugnet@gmail.com>
2020-02-14 00:02:16 +0100

    prod version with trigger

--- stitch/Maxime/import.js
+++ stitch/Maxime/import.js
@@ -1,28 +1,33 @@
 exports = async function () {
-    const statistics = context.services.get("mongodb-atlas").db("coronavirus").collection("statistics");
-    statistics.deleteMany({}).then(result => console.log(JSON.stringify(result)));
+    const temp = context.services.get("mongodb-atlas").db("coronavirus").collection("temp");
+    await temp.deleteMany({});
 
     const csv_confirmed = await context.http.get({url: "https://raw.githubusercontent.com/CSSEGISandData/2019-nCoV/master/time_series/time_series_2019-ncov-Confirmed.csv"});
     const csv_deaths = await context.http.get({url: "https://raw.githubusercontent.com/CSSEGISandData/2019-nCoV/master/time_series/time_series_2019-ncov-Deaths.csv"});
     const csv_recovered = await context.http.get({url: "https://raw.githubusercontent.com/CSSEGISandData/2019-nCoV/master/time_series/time_series_2019-ncov-Recovered.csv"});
 
-    const dates = extract_dates_from_headers(csv_confirmed.body.text());
-    let docs = generate_docs_template(csv_confirmed.body.text());
+    const csv_confirmed_lines = extract_lines(csv_confirmed.body.text());
+    const csv_deaths_lines = extract_lines(csv_deaths.body.text());
+    const csv_recovered_lines = extract_lines(csv_recovered.body.text());
+
+    const dates = extract_dates_from_headers(csv_confirmed_lines);
+    const docs = generate_docs_template(csv_confirmed_lines);
 
     for (let column_index = 0; column_index < dates.length; column_index++) {
         let current_docs = docs.slice();
-        import_csv_update_docs(current_docs, dates[column_index], column_index, csv_confirmed.body.text(), "confirmed");
-        import_csv_update_docs(current_docs, dates[column_index], column_index, csv_deaths.body.text(), "deaths");
-        import_csv_update_docs(current_docs, dates[column_index], column_index, csv_recovered.body.text(), "recovered");
-        await statistics.insertMany(current_docs);
+        import_csv_update_docs(current_docs, dates[column_index], column_index, csv_confirmed_lines, "confirmed");
+        import_csv_update_docs(current_docs, dates[column_index], column_index, csv_deaths_lines, "deaths");
+        import_csv_update_docs(current_docs, dates[column_index], column_index, csv_recovered_lines, "recovered");
+        remove_empty_states(current_docs);
+        await temp.insertMany(current_docs);
     }
-    statistics.updateMany({state: ""}, {$unset: {state: 1}});
-    return 0;
+
+    console.log("Renaming collection \"temp\" to \"statistics\".");
+    await temp.aggregate([{$out: "statistics"}]).next();
+    return temp.deleteMany({});
 };
 
-function generate_docs_template(csv) {
-    csv = csv.replace(/Mainland /g, "");
-    const lines = csv.split("\n");
+function generate_docs_template(lines) {
     let docs = [];
 
     for (let i = 1; i < lines.length; i++) {
@@ -45,10 +50,7 @@ function generate_docs_template(csv) {
     return docs;
 }
 
-function import_csv_update_docs(docs, date, column_index, csv, field) {
-    csv = csv.replace(/Mainland /g, "");
-    const lines = csv.split("\n");
-
+function import_csv_update_docs(docs, date, column_index, lines, field) {
     for (let i = 1; i < lines.length; i++) {
         let current_line = lines[i];
         const state = extract_state(current_line);
@@ -67,19 +69,28 @@ function import_csv_update_docs(docs, date, column_index, csv, field) {
     }
 }
 
-function extract_dates_from_headers(csv) {
-    const lines = csv.split("\n");
+function extract_lines(csv) {
+    return csv.replace(/Mainland /g, "").trim().split("\n");
+}
+
+function remove_empty_states(docs) {
+    docs.forEach(doc => {
+        if (doc.state === "") {
+            delete doc.state;
+        }
+    })
+}
+
+function extract_dates_from_headers(lines) {
     return lines[0].split(",").slice(4);
 }
 
 function extract_state(line) {
     let state;
     if (line.indexOf("\"") === 0) {
-        const tokenList = line.split("\"");
-        state = tokenList[1];
+        state = line.split("\"")[1];
     } else {
-        const tokenList = line.split(",");
-        state = tokenList[0];
+        state = line.split(",")[0];
     }
     return state;
 }
@@ -87,7 +98,7 @@ function extract_state(line) {
 function shift_line(line, times = 1) {
     for (let i = 0; i < times; i++) {
         if (line.indexOf("\"") === 0) {
-            line = (line.split("\""))[2].substring(1);
+            line = line.split("\"")[2].substring(1);
         } else {
             line = line.split(",").slice(1).join(",");
         }
